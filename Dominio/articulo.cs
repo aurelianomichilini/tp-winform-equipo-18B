a91b2078ab19d459ad938b50511c524978a83f1e
Aureliano Michilini <aurelianomichilini@hotmail.com>
2026-04-17 20:11:30 -0300

    primer commit

---
+++ Dominio/articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class articulo
+    {
+        public int codigo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public marca IdMarca { get; set; }
+
+        public categoria IdCategoria { get; set; }
+
+        public float precio { get; set; }
+    }
+}
---
+++ TPWinForm_Equipo18B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+
+
+namespace TPWinForm_Equipo18B
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
